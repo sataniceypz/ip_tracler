274f7bdc485b3c0fe3cf52a002da5bb88b966e25
Eypz <105191630+sataniceypz@users.noreply.github.com>
2024-12-21 15:50:00 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
+    TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID,
+    IP_REGISTRY_API_KEY: process.env.IP_REGISTRY_API_KEY
+};
